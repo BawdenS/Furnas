502534f99ee262a72e79f5ec9dab9e5de12ac9e4
BawdenS <mathelatebo@gmail.com>
2023-03-09 17:51:40 -0300

    Correção Erro
    Correção do Erro.

--- hydrolib/object_manager.py
+++ hydrolib/object_manager.py
@@ -5,6 +5,6 @@ from  .module_simulator import *
 from  .module_varied import *
 
 class Hidr_Manager:
-    def __init__(self, caminho_arq):
+    def __init__(self, caminho_arq:str ):
         self.Arquivo_Base =  loader.load(caminho_arq)
         self.df = cleaner.clean(self.Arquivo_Base)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from hydrolib.object_manager import *
 # cleaner.clean()
-# Gerenciador = Hidr_Manager("Sem_Alteracoes.xlsx")
-# print(Gerenciador.df)
+Gerenciador = Hidr_Manager("Sem_Alteracoes.xlsx")
+print(Gerenciador.df)
